9ee5b6159e66ac7a7b7c35f8b5d24f08f8bf3fc3
Ashutosh Swamy <66406048+CoolCoder2020@users.noreply.github.com>
2020-06-11 23:23:14 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,44 @@
-var drawing = [];
+var database;
+
+var drawing =[];
 
 function setup() {
   createCanvas(400,400);
 }
+database = firebase.database;
+var ref = database.ref('drawings');
+ref.on('value', gotData , errData);
 
 function draw() {
-  background(0);  
+  background(0);
 
-  if(mouseIsPressed){
-    var point = {
-      x : mouseX,
-      y : mouseY
+if(mouseIsPressed){
+  var point = {
+    x : mouseX,
+    y : mouseY
   }
-  
   drawing.push(point);
+}
+
+fill(255);
+textSize(20);
+text("DRAW YOUR IMAGINATION",75,30);
+
+beginShape();
+stroke(255);
+strokeWeight(4);
+noFill();
+for(var i = 0; i < drawing.length; i++){
+  vertex(drawing[i].x , drawing[i].y);
 
 }
-  beginShape();
-  stroke(255);
-  strokeWeight(4);
-  noFill();
-  for(var i = 0; i < drawing.length; i++) {
-  vertex(drawing[i].x , drawing[i].y)
+endShape();
 }
-  drawSprites();
+
+function gotData(){
+  var drawing = data.val();
+}
+
+function errData(){
+  console.log(err);
 }
